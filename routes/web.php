7eefb5da9c1acabf1d50917272ab15a680e3ae28
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-10-09 16:00:16 +0200

    do routes

--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,11 @@ Route::middleware(['auth', 'verified'])
 
 		Route::get("/projects", [ProjectController::class, "index"])->name("projects.index");
 		Route::get("/projects/{project}", [ProjectController::class, "show"])->name("projects.show");
+
+        Route::get("/projects/{project}/edit", [ProjectController::class, "edit"])->name("projects.edit");
+        Route::patch("/projects/{project}", [ProjectController::class, "update"])->name("projects.update");
+
+        Route::delete("/projectss/{projects}", [ProjectsController::class, "destroy"])->name("projectss.destroy");
 });
 
 require __DIR__ . '/auth.php';
